9c7463196ce92dd0aca0ef072cf2add3e9057d9d
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2021-06-27 03:44:01 +0530

    extensions

---
+++ src/components/Extension/Extension.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, Button, CssBaseline, Grid, Toolbar, Container, Link } from '@material-ui/core';
+import './styles.css';
+
+
+const Extension = () => {
+
+    return (
+        <>
+            <Typography variant="h2" align="center" style={{ color: "#2F5D62" }} gutterBottom>
+                Chrome Extensions
+            </Typography>
+            {/* <Typography variant="h6" align="center" color="textSecondary" paragraph>
+                hello everyone, this is a photo album and I m trying ti asdfghjkl zxcvbnm.
+            </Typography> */}
+            <div className="main">
+                <div className="submain">
+                    <img
+                        src={require("../../images/lec.jpeg").default}
+
+                        alt="headerImage"
+                        className="images"
+                    />
+
+                    <Typography variant="h3" align="center" color="textSecondary" paragraph style={{ color: "#2F5D62" }}>
+                        Krypto Lectures
+                    </Typography>
+
+
+
+                </div>
+                <div className="submain">
+                    <Typography variant="h5" align="center" color="textSecondary" paragraph style={{ color: "#558776" }}>
+                        Store your most used Zoom, Meet and Teams links and join your online classes at your own ease
+                    </Typography>
+                    <img
+                        src={require("../../images/reading.svg").default}
+                        style={{ width: "450px" }}
+                        alt="headerImage"
+                    />
+                </div>
+                <div className="submain">
+                    <img
+                        src={require("../../images/block.png").default}
+                        className="images"
+                        alt="headerImage"
+                    />
+                    <Typography variant="h3" align="center" color="textSecondary" paragraph style={{ color: "#2F5D62" }}>
+
+                        KryptoKnights block
+
+
+                    </Typography>
+                </div>
+                <div className="submain">
+                    <Typography variant="h5" align="center" color="textSecondary" paragraph style={{ color: "#558776" }}>
+
+                        Block social media sites using this chrome extension and boost up your productivity!
+                    </Typography>
+                    <img
+                        src={require("../../images/laptop.svg").default}
+                        style={{ width: "450px" }}
+
+                        alt="headerImage"
+                    />
+
+
+                </div>
+            </div>
+
+
+
+
+
+
+
+        </>
+
+    );
+}
+
+export default Extension;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <div className="row">
           <div className="col-10 mx-auto">
 
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light" >
               <div className="container-fluid">
                 <NavLink className="navbar-brand" to="/">KryptoKnights </NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,6 +18,9 @@ const Navbar = () => {
                   <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li className="nav-item">
                       <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink activeClassName="menu_active" className="nav-link" to="/extension">Extension</NavLink>
                     </li>
                     <li className="nav-item">
                       <NavLink activeClassName="menu_active" className="nav-link" to="/todolist">ToDoList</NavLink>
