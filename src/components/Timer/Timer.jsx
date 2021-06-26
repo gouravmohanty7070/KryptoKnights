ff156013bf6a10b907bad1f2a61ec2c51ff5f8ff
rituraj2000 <83244005+rituraj2000@users.noreply.github.com>
2021-06-26 21:15:43 +0530

    Timer changes

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,100 +1,65 @@
-import React from 'react'
-
-import './style.css'
+import React, {useState} from 'react';
+import DisplayComponent from './DisplayComponent';
+import BtnComponent from './BtnComponent';
+import './style.css';
 
 function Timer() {
+  const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+  // Not started = 0
+  // started = 1
+  // stopped = 2
 
-    const[time, setTime] = React.useState(0)
-  
-    const[timerOn, setTimerOn] = React.useState(false)
-  
-  
-    React.useEffect(() => {
-  
-        let interval = null;
-  
-        if(timerOn){
-  
-            interval = setInterval(() => {
-  
-  
-               setTime(prevTime => prevTime+10)
-  
-  
-  
-  
-  
-            }, 10)        
-            
-  
-        }else{
-  
-  
-            clearInterval(interval)
-  
-  
-  
-        }
-        
-        
-         return() => clearInterval(interval)
-  
-  
-  
-    }, [timerOn])
-  
-  
-  
-    return (
-  
-       
-           <div className="App">
-  
-  
-               <div></div>
-             <div>
-                 
-                 
-                 
-                 <span>{("0"+Math.floor(time/60000) % 60).slice(-2)}:</span>
-                 <span>{("0"+Math.floor(time/1000) % 60).slice(-2)}:</span>
-                 <span>{("0"+(time/10) % 100).slice(-2)}</span>
-                 
-              
-  
-  
-  
-  
-  
-  
-             </div>
-  
-  
-             <div>
-  
-                  <button onClick={() => setTimerOn(true)}>GO!</button>
-                  <button onClick={() => setTimerOn(false)}>Pause</button>
-                  <button onClick={() => setTimerOn(true)}>Resume</button>
-                  <button onClick={() => setTime(0)}>Reset</button>
-                        
-  
-  
-  
-  
-  
-             </div>
-  
-  
-  
-  
-  
-  
-           </div>
-  
-         
-  
-  
-    );
-  }
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
+
+  var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
+
+  const run = () => {
+    if(updatedM === 60){
+      updatedH++;
+      updatedM = 0;
+    }
+    if(updatedS === 60){
+      updatedM++;
+      updatedS = 0;
+    }
+    if(updatedMs === 100){
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH});
+  };
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({ms:0, s:0, m:0, h:0})
+  };
+
+  const resume = () => start();
+
+
+  return (
+    <div className="main-section">
+     <div className="clock-holder">
+          <div className="stopwatch">
+               <DisplayComponent time={time}/>
+               <BtnComponent status={status} resume={resume} reset={reset} stop={stop} start={start}/>
+          </div>
+     </div>
+    </div>
+  );
+}
 
 export default Timer;
