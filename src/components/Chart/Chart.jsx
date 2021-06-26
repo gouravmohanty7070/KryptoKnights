ee31894d18fe0d62bbe70c9298832415eee619f8
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2021-06-26 19:55:55 +0530

    chart.js

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Line, Bar, Radar } from 'react-chartjs-2';
+import './styles.css';
+import LineChart from './LineChart';
+import BarChart from './Bar';
+import RadarChart from './Radar';
+import Typography from '@material-ui/core/Typography';
+
+
+
+function Chart() {
+
+
+
+    const data = {
+        labels: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'April',
+            'May'
+        ],
+        datasets: [
+            {
+                label: 'Sales 2020 (M)',
+                data: [3, 2, 2, 1, 5],
+                borderColor: ['rgba(255, 206, 86, 0.2)'],
+                backgroundColor: ['rgba(255, 206, 86, 0.2)'],
+                pointBackgroundColor: 'rgba(255, 206, 86, 0.2)',
+                pointBorderColor: 'rgba(255, 206, 86, 0.2)'
+            },
+            {
+                label: 'Sales 2019 (M)',
+                data: [1, 3, 2, 2, 3],
+                borderColor: ['rgba(54, 162, 235, 0.2)'],
+                backgroundColor: ['rgba(54, 162, 235, 0.2)'],
+                pointBackgroundColor: 'rgba(54, 162, 235, 0.2)',
+                pointBorderColor: 'rgba(54, 162, 235, 0.2)'
+            }
+        ]
+    }
+
+    const options = {
+        title: {
+            display: true,
+            text: 'Line Chart'
+        },
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        min: 0,
+                        max: 5,
+                        stepSize: 1.5
+                    }
+                }
+            ]
+        }
+    }
+
+
+
+
+    return (
+        <>
+            <LineChart />
+            <Typography variant="h6" gutterBottom>
+                heading
+            </Typography>
+            {/* <BarChart /> */}
+            <RadarChart />
+            <Typography variant="h6" gutterBottom>
+                heading
+            </Typography>
+        </>
+    )
+}
+
+export default Chart
---
+++ src/components/Chart/Radar.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Radar } from 'react-chartjs-2';
+
+function RadarChart() {
+    const data = {
+        labels: [
+            'Maths',
+            'Chemistry',
+            'Physics',
+            'Social Studies',
+            'Computer Science'
+        ],
+        datasets: [
+            {
+                label: 'Sales 2020 (M)',
+                data: [3, 2, 2, 1, 5],
+                fill: true,
+                borderColor: ['rgb(255, 99, 132)'],
+                backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+                pointBackgroundColor: 'rgb(255, 99, 132)',
+                pointBorderColor: 'rgb(255, 99, 132)',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgb(255, 99, 132)'
+
+            },
+            {
+                label: 'Sales 2019 (M)',
+                data: [1, 3, 2, 2, 3],
+                fill: true,
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgb(54, 162, 235)',
+                pointBackgroundColor: 'rgb(54, 162, 235)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgb(54, 162, 235)'
+            }
+        ]
+    }
+
+    const options = {
+        title: {
+            display: true,
+            text: 'Line Chart'
+        },
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        min: 0,
+                        max: 5,
+                        stepSize: 1.5
+                    }
+                }
+            ]
+        }
+    }
+
+
+
+
+    return (
+        <div className="chartapp">
+            <div className="chart">
+                <Radar data={data} options={options} />
+
+
+
+            </div>
+        </div>
+    )
+}
+
+export default RadarChart
